1f9762df85979fb0fc848809e9cefbf19b413612
Veronika <nikarinkevich123@gmail.com>
2023-11-24 17:09:06 +0300

    project

--- game.js
+++ game.js
@@ -1,5 +1,7 @@
 const question = document.getElementById("question");
 const choices = Array.from(document.getElementsByClassName("choice-text"));
+const questionCounterText = document.getElementById("question");
+const scoreText = document.getElementById("score");
 
 let currentQuestion = {};
 let acceptingAnswers = false;
@@ -53,6 +55,7 @@ getNewQuestion = () => {
     return window.location.assign("/end.html");
   }
   questionCounter++;
+  questionCounterText.innerText = `${questionCounter}/${MAX_QUESTIONS}`;
   const questionIndex = Math.floor(Math.random() * availableQuestions.length);
   currentQuestion = availableQuestions[questionIndex];
   question.innerText = currentQuestion.question;
@@ -77,15 +80,23 @@ choices.forEach(choice => {
     const classToAppply =
       selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
 
+    if (classToAppply === "correct") {
+      incrementScore(CORRECT_BONUS);
+    }
 
     selectedChoice.parentElement.classList.add(classToAppply);
 
     setTimeout(() => {
       selectedChoice.parentElement.classList.remove(classToAppply);
       getNewQuestion();
-    })
+    }, 1000);
   });
 });
+
+incrementScore = num => {
+  score += num;
+  scoreText.innerText = score;
+}
 
 startGame();
 
